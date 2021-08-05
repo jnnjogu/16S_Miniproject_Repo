d4595e42ec6b3446e8a2e0f7e54d59cd4fe74922
Johny <76898485+jnnjogu@users.noreply.github.com>
2021-08-05 16:37:30 +0300

    removed the irrelevant scripts
    removed the irrelevant scripts

--- old.sh
+++
@@ -1,6 +0,0 @@
-
-for file in *.gz;
-do
-	sample=$(echo ${file} |sed 's/_L001_R*//')
-	cutadapt --interleaved -q 20 -o cd ../Trimmed/${sample}_R1.fastq -p cd ../Trimmed/${sample}_R2.fastq *R1* *R2*
-done
